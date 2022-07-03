532c27828fab60ce92213192985df02c5f3995f4
unknown <jadentr.44@gmail.com>
2022-07-03 13:32:34 -0500

    will have color based on hour

--- script.js
+++ script.js
@@ -11,39 +11,53 @@ let Schedule = [{
   Time:12,
   task:""
 },{
-  Time:1,
+  Time:13,//1
   task:""
 },{
-  Time:2,
+  Time:14,//1
   task:""
 },{
-  Time:3,
+  Time:15,//1
+  task:"feed"
+},{
+  Time:16,//1
   task:""
 },{
-  Time:4,
-  task:""
-},{
-  Time:5,
+  Time:17,//5
   task:""
 }]
 
 
 let date = moment().format('MM,Do,YY')
 console.log(date)
-let hour = moment().format('h')
+let hour = moment().format('H')
 console.log(hour)
-let saveIconHtml
+let saveIconHtml = `<i class="fa fa-save"></i>`
 
 Schedule.forEach(e =>{
+  let rowColor = "red"
+  if(e.Time > hour){rowColor = "green"}
+  if(e.Time < hour){rowColor = "gray"}
+
+ 
+  console.log(`current hour:${hour} vs ${e.Time}`)
   let rowDiv = $('<div>')
   rowDiv.attr("class","row")
 
-  let AMPM = "pm"
-  if(e.Time < 12){AMPM = 'am'}
-  
-  rowDiv.html(`<div class="hour col-lg-2"><h3>${e.Time}${AMPM}</h3></div>
-  <div class="description col-lg-8">${e.task}</div>
-  <div class="saveBtn col-lg-2"><i class="fa-solid fa-floppy-disk"></i></div>
-</div>`)
+  let hourDiv = $('<div>')
+  hourDiv.attr('class','hour col-lg-2')
+  hourDiv.text(moment(e.Time,'H').format('h'))
+
+  let taskDiv = $('<div>')
+  taskDiv.text(e.task)
+  taskDiv.attr('class',"description col-lg-8")
+  taskDiv.css("background-color", rowColor);
+
+  let iconDiv = $('<div>')
+  iconDiv.attr('class','saveBtn col-lg-2')
+  iconDiv.html(saveIconHtml)
+
+rowDiv.append(hourDiv,taskDiv,iconDiv)
 $('.container').append(rowDiv)
+  
 })
